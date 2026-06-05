e4c32a0cd0f34fe1758eec8f661ed9845431b138
LanRhyme <xiao_ren233@foxmail.com>
2026-06-06 00:45:31 +0800

    feat: add semi-transparent black overlay mask to contour background

--- src/components/Background3D.vue
+++ src/components/Background3D.vue
@@ -272,5 +272,9 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div ref="containerRef" class="fixed inset-0 pointer-events-none z-[-1] opacity-70"></div>
+  <div class="fixed inset-0 pointer-events-none z-[-1]">
+    <div ref="containerRef" class="absolute inset-0 opacity-70"></div>
+    <!-- Black semi-transparent overlay mask -->
+    <div class="absolute inset-0 bg-black/50"></div>
+  </div>
 </template>
